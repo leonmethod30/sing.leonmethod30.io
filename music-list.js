3273e069a57a4b7784b4c0c942082704407a65bf
leons <leons@ROG>
2026-06-01 20:16:24 -0700

    separate files

---
+++ music-list.js
@@ -0,0 +1,8 @@
+const availableMusicFiles = [
+    "because-of-you.xml",
+    "man-i-need.xml",
+    "together-intro.xml",
+    "when-we-rise.xml",
+    "when-you-want.xml",
+    "unwritten.xml"
+];
